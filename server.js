61ba1d3dec50518fcd2338cffd5aa1fbc9e91b7f
Franck Asket <32412954+asketsystem@users.noreply.github.com>
2022-10-10 00:36:19 +0000

    GraphQL server structure

--- server.js
+++ server.js
@@ -1,16 +1,6 @@
-import { ApolloServer, gql } from "apollo-sever";
-
-const typeDefs = gql`
-  type Query {
-    greet: String
-  }
-`;
-
-const resolvers = {
-  Query: {
-    greet: () => "HI NOON",
-  },
-};
+import { ApolloServer, gql } from "apollo-server";
+import typeDefs from "./typeDefs.js";
+import resolvers from "./resolvers.js";
 
 const server = new ApolloServer({ typeDefs, resolvers });
 
